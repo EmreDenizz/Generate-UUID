28493f77a067c613ccf3d0f76b8e47dcf8867b79
emre <emre.deniz@outlook.com>
2023-03-27 14:34:29 +0300

    Small fixes

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * @author Emre Deniz <https://github.com/EmreDenizz>
+*/
 
 // Generate UUID string
 $UUID = sprintf('%04x%04x-%04x-%04x-%04x', mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0x0fff) | 0x4000, mt_rand(0, 0x3fff) | 0x8000) . "-" . substr(uniqid(), - 12);
